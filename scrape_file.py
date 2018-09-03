e7fe205f7a68d6302805c014c53a84e409132ad7
johncoleman83 <djc_fileno@davidjohncoleman.com>
2018-09-03 12:10:31 -0700

    created new app to scrape files

--- scrape_file.py
+++ scrape_file.py
@@ -8,68 +8,61 @@ WIP WORK IN PROGRESS
 import re
 import requests
 import sys
+import os.path
+import datetime
+import random
+import queue
 
-
-all_links = {}
+broken_links = {}
 domain_links_q = queue.Queue()
-external_and_image_links_q = queue.Queue()
 TIMEOUT = (3, 10)
+OUTPUT_FILE = './broken_links_' + str(random.random()).split('.')[1]
 
 
-def error_check_and_init_main_domain():
+def error_check_and_init_main_file():
     """
     checks errors and saves original_domain
     """
     global original_domain
     if len(sys.argv) != 2:
         print("Usage:", file=sys.stderr)
-        print("$ ./scrape_url.py [URL TO BE SCAPED]", file=sys.stderr)
+        print("$ ./scrape_url.py [FILE TO BE SCRAPED]", file=sys.stderr)
         sys.exit(1)
-    url = sys.argv[1]
-    if 'http' not in url or '://' not in url:
-        print("please use a valid HTTP URL", file=sys.stderr)
+    INPUT_FILE = sys.argv[1]
+    if not os.path.isfile(URL_FILE):
+        print("please use a valid file", file=sys.stderr)
         sys.exit(1)
-    url = add_terminating_slash_to_url(url)
-    pattern = re.compile("(\.{1}.*\.{1}.*\/.*)")
-    m = re.search(pattern, url)
-    if m is None:
-        pattern = re.compile("(:\/\/.*\.{1}.*\/.*)")
-        m = re.search(pattern, url)
-        if m is None:
-            print("please use a valid HTTP URL", file=sys.stderr)
-            sys.exit(1)
-        original_domain = m.groups()[0][3:-1]
-    else:
-        original_domain = m.groups()[0][1:-1]
-    return url
+    return INPUT_FILE
 
 
-def add_terminating_slash_to_url(url):
+def read_file_add_to_queue(INPUT_FILE):
     """
-    adds terminating slash if necessary to main input URL
+    reads links from input file and adds them to a queue
     """
-    if url[-1] != '/':
-        url += '/'
-    return url
+    with open(INPUT_FILE, "r", encoding="utf-8") as open_file:
+        for i, new_link in enumerate(open_file):
+            if url_is_new(new_link):
+                domain_links_q.put(new_link)
 
-def url_has_been_reviewed(url):
+
+def url_is_new(url):
     """
     checks if URL exists in reviewed storage of URLs
     """
     if 'www.' in url:
         i = url.index('www.')
         new = "{}{}".format(url[:i], url[i + 4:])
-        if new in all_links:     return True
+        if new in broken_links:     return False
     else:
         i = url.index('://')
         new = "{}www.{}".format(url[:i + 3], url[i + 3:])
-        if new in all_links:     return True
-    if url in all_links:         return True
-    elif url + '/' in all_links: return True
-    elif url[:-1] in all_links:  return True
-    else:                        return False
+        if new in broken_links:     return False
+    if url in broken_links:         return False
+    elif url + '/' in broken_links: return False
+    elif url[:-1] in broken_links:  return False
+    else:                        return True
 
-def scrape_url_from_original_domain(url):
+def check_url_and_add_to_lists(url):
     """
     scrapes url that is from main domain website
     """
@@ -78,30 +71,11 @@ def scrape_url_from_original_domain(url):
     except Exception as e:
         print(e)
         return
-    all_links[url] = r.status_code
-    if (r.headers['Content-Type'] != 'text/html; charset=UTF-8'
-        or all_links[url] >= 400):
-        return
-    soup = BeautifulSoup(r.text, 'html.parser')
-    pattern = re.compile("(http.*\:\/\/.*\.+.*\/.*)")
-    for link in soup.find_all('a'):
-        new_url = link.get('href', None)
-        if new_url is None: continue
-        m = re.search(pattern, new_url)
-        if m is None or url_has_been_reviewed(new_url):
-            continue
-        all_links[new_url] = None
-        if original_domain in new_url:
-            domain_links_q.put(new_url)
-        else:
-            external_and_image_links_q.put(new_url)
-    for link in soup.find_all('img'):
-        new_url = link.get('src')
-        m = re.search(pattern, new_url)
-        if m is None or url_has_been_reviewed(new_url):
-            continue
-        all_links[new_url] = None
-        external_and_image_links_q.put(new_url)
+    if r.status_code > 302:
+        broken_links[url] = r.status_code
+    if r.status_code == 302:
+        broken_links[url] = r.url
+    return
 
 
 def domain_links_loop():
@@ -110,46 +84,33 @@ def domain_links_loop():
     """
     while domain_links_q.empty() is False:
         url = domain_links_q.get()
-        scrape_url_from_original_domain(url)
-
-def external_and_image_head_request(url):
-    """
-    makes head request for external and image URL inputs
-    """
-    try:
-        r = requests.head(url, allow_redirects=True, timeout=TIMEOUT)
-    except Exception as e:
-        print(e)
-        return
-    all_links[url] = r.status_code
-
-def external_and_image_links_loop():
-    """
-    loops and makes head request to all queue'd URL's
-    """
-    while external_and_image_links_q.empty() is False:
-        url = external_and_image_links_q.get()
-        external_and_image_head_request(url)
+        check_url_and_add_to_lists(url)
 
 
-def print_results():
+def write_results_to_file():
     """
-    final printing of results
+    final writing of results
     """
-    for l, s in all_links.items():
-        if s >= 400:
-            print(l, s)
+    FIRST_LINE = """TIME: {}
+        link                  -                   status
+""".format(str(datetime.datetime.now()))
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as open_file:
+        open_file.write(FIRST_LINE)
+        for l, s in broken_links.items():
+            line = "{} - {}\n".format(l, s)
+            open_file.write(line)
+
 
 def main_app():
     """
     completes all tasks of the application
     """
-    url = error_check_and_init_main_domain()
-    all_links[url] = None
-    domain_links_q.put(url)
+    INPUT_FILE = error_check_and_init_main_file()
+    # all_links[url] = None
+    # domain_links_q.put(url)
+    read_file_add_to_queue(INPUT_FILE)
     domain_links_loop()
-    external_and_image_links_loop()
-    print_results()
+    write_results_to_file()
 
 if __name__ == "__main__":
     """
