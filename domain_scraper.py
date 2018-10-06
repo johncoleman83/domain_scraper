03a5fae13c4739dbc2dd11eae19cbf9055d4d93f
John Coleman <jcoleman@pagerduty.com>
2018-10-05 22:56:41 -0700

    removed too much whitespace

--- domain_scraper.py
+++ domain_scraper.py
@@ -21,17 +21,13 @@ usage:
 $ python3 optlis_option arg
 """
 	longopts = [ "check=", "extract=", "scrape=" ]
-
 	optlist, args = getopt.getopt( args, '', longopts )
-
 	opt = optlist[0]
 
 	func = opt[0].replace( "--", "" ) + "_" + opt[1].replace( "-", "_" ) + ".py"
-
 	path = os.path.join( os.path.dirname(os.path.abspath(__file__)), "modules", func )
 
 	cmd = "python " + path + " " + args[0]
-
 	os.system( cmd )
 
 if __name__ == "__main__":
